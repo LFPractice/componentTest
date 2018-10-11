be65f114af0777f18bd071024333cae3303e17f5
WangLinfang <1261876974@qq.com>
2018-10-11 13:08:59 +0800

    项目初始化

---
+++ CocoaPodsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CocoaPodsTest
+//
+//  Created by 王林芳 on 2018/10/10.
+//  Copyright © 2018年 王林芳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
